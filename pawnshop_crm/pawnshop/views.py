7659bdbc3c53d1248c1c0f336c9968bbed3d8b9b
wwhite_darkness <olimpsdu@gmail.com>
2019-09-18 17:45:05 +0600

    #5 Добавлен шаблон credit/list.html, реализован CreditListView

--- pawnshop_crm/pawnshop/views.py
+++ pawnshop_crm/pawnshop/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.views.generic import *
 
 
-class CreditListView(ListView):
-    pass
+class CreditListView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'credit/list.html')
