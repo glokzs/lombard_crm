6f95449e143eea12b623bbc6d46585c868f2d924
wwhite_darkness <olimpsdu@gmail.com>
2019-09-19 21:14:19 +0600

    #22 Добавлена модель Subcategory в приложение pawnshop

--- pawnshop_crm/pawnshop/admin.py
+++ pawnshop_crm/pawnshop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Client, ConfirmDocument, Category
+from .models import Client, ConfirmDocument, Category, Subcategory
 
 
 admin.site.register(Client)
 admin.site.register(ConfirmDocument)
 admin.site.register(Category)
+admin.site.register(Subcategory)
--- pawnshop_crm/pawnshop/models.py
+++ pawnshop_crm/pawnshop/models.py
@@ -169,3 +169,39 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(
+        'pawnshop.Category',
+        related_name='subcategories',
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name='Родительская категория',
+        on_delete=models.CASCADE
+    )
+    subcategory = models.ForeignKey(
+        'pawnshop.Subcategory',
+        related_name='subcategories',
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name='Родительская субкатегория',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        verbose_name='Имя подкатегории',
+        max_length=100,
+        null=False,
+        blank=False,
+        default=None
+    )
+
+    class Meta:
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
+
+    def __str__(self):
+        return self.name
+
