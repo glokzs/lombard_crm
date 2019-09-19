a934511027ae764543327a3d6c56d693aec1c47f
wwhite_darkness <olimpsdu@gmail.com>
2019-09-19 20:42:38 +0600

    #21 Создана модель Category и зарегистрирована в админке

--- pawnshop_crm/pawnshop/admin.py
+++ pawnshop_crm/pawnshop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Client, ConfirmDocument
+from .models import Client, ConfirmDocument, Category
 
 
 admin.site.register(Client)
 admin.site.register(ConfirmDocument)
+admin.site.register(Category)
