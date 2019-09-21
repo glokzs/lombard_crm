43b1ef96d67415cd3eccde6d8c61aadcb0924969
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-09-21 17:03:57 +0600

    #26 Добавлена модель PledgeItem

---
+++ pawnshop_crm/pawnshop/models/PledgeItem.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+class PledgeItem(models.Model):
+    subcategory = models.ForeignKey(
+        'pawnshop.Subcategory',
+        related_name='pledge_items',
+        null=False,
+        blank=False,
+        default=None,
+        verbose_name='Относится к подкатегории',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=100,
+        null=False,
+        blank=False,
+        default=None
+    )
+    price = models.DecimalField(
+        verbose_name='Оценочная стоимость',
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        default=None
+    )
+    description = models.CharField(
+        verbose_name='Описание',
+        max_length=200,
+        null=False,
+        blank=False,
+        default=None
+    )
+    note = models.CharField(
+        verbose_name='Примечание',
+        max_length=100,
+        null=True,
+        blank=True,
+        default=None
+    )
+    image = models.ImageField(
+        verbose_name='Фотография',
+        null=True,
+        blank=True,
+        default=None
+    )
+
+    class Meta:
+        verbose_name = 'Предмет залога'
+        verbose_name_plural = 'Предметы залога'
+
+    def __str__(self):
+        return f'{self.subcategory.name} - \'{self.name}\' - {self.price}'
--- pawnshop_crm/pawnshop/models/__init__.py
+++ pawnshop_crm/pawnshop/models/__init__.py
@@ -4,3 +4,4 @@ from .Category import *
 from .Subcategory import *
 from .Criteria import *
 from .CriteriaPledgeItem import *
+from .PledgeItem import *
