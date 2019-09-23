1f1f3fb5430be2a50632af004b6e6d9d6f955cfe
Положенцев Евгений <apollo64@gmail.com>
2019-09-23 17:57:59 +0600

    #33 Добавить user_type. Добавлено, так же добавлен middle_name.

--- pawnshop_crm/accounts/models/User.py
+++ pawnshop_crm/accounts/models/User.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+USER_TYPE_CHOICES = (
+    ("Administrator", "Administrator"),
+    ("Cashier", "Cashier"),
+    ("Auditor", "Auditor"),
+    )
 
 class User(AbstractUser):
     initial_password_changed_at = models.DateTimeField(
@@ -8,3 +13,18 @@ class User(AbstractUser):
         blank=True,
         default=None,
         verbose_name='Пароль изменен')
+    middle_name = models.CharField(
+        max_length=30,
+        null = True,
+        blank=True,
+        default = None,
+        verbose_name='Отчество'
+    )
+    user_type = models.CharField(
+        max_length= 1,
+        null = False,
+        blank = False,
+        default = None,
+        choices= USER_TYPE_CHOICES,
+        verbose_name='тип прав'
+    )
--- pawnshop_crm/accounts/views.py
+++ pawnshop_crm/accounts/views.py
@@ -1,49 +1,10 @@
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from django.shortcuts import redirect, render
 
 
-#
-# def user_login_view(request):
-#     context = {}
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-#         next_url = request.POST.get('next')
-#         user = authenticate(request, email=email, password=password)
-#         if user is not None:
-#             login(request, user)
-#             if next_url:
-#                 return redirect(next_url)
-#             return redirect('pawnshop:credits')
-#         else:
-#             context['has_error'] = True
-#             context['next'] = next_url
-#             context['email'] = email
-#     else:
-#         context = {'next': request.GET.get('next')}
-#     return render(request, 'registration/login.html', context=context)
 from accounts.models import User
-
-
-# def user_login_view(request):
-#     context = {}
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         username = User.objects.get(email__exact=f'{email}').username
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=username, password=password)
-#         print(f'user {user}')
-#         print (f'when user changed password: {user.initial_password_changed_at}')
-#         if user:
-#             login(request, user)
-#             return redirect('accounts:login')
-#         else:
-#             context['has_error'] = True
-#     return render(request, 'user/login.html', context=context)
-
 
 
 def user_login_view(request):
@@ -52,14 +13,11 @@ def user_login_view(request):
         email = request.POST.get('email')
         try:
             username = User.objects.get(email__exact=f'{email}').username
-            # context['has_error'] = True
         except:
             context['has_error'] = True
             return render(request, 'user/login.html', context=context)
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        print(f'user {user}')
-        print (f'when user changed password: {user.initial_password_changed_at}')
         if user:
             login(request, user)
             return redirect('accounts:login')
@@ -68,11 +26,39 @@ def user_login_view(request):
     return render(request, 'user/login.html', context=context)
 
 
-
 def logout_view(request):
     logout(request)
     return redirect('accounts:login')
 
+
+
+# class UserListAjaxView(View):
+#     def get(self, request, *args, **kwargs):
+#         query = self.request.GET.get('query')
+#         first_name_query = Q(first_name__icontains=query)
+#         middle_name_query = Q(first_name__icontains=query)
+#         last_name_query = Q(last_name__icontains=query)
+#
+#         iin_query = Q(confirm_document__iin__icontains=query)
+#         users = User.objects.filter(first_name_query | last_name_query | iin_query)
+#
+#         data = {
+#             'users': []
+#         }
+#         if not query:
+#             return JsonResponse(data)
+#
+#         for user in users:
+#             user_object = {
+#                 'pk': user.pk,
+#                 'first_name': user.first_name,
+#                 'last_name': user.last_name,
+#                 'middle_name': user.middle_name,
+#                 'birth_date': user.birth_date,
+#                 'iin': user.confirm_document.iin
+#             }
+#             data['users'].append(user_object)
+#         return JsonResponse(data)
 
 # def register_view(request, *args, **kwargs):
 #     if request.method == 'POST':
