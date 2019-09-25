18dae35df4ee2222500e66f546afed2012f6501a
Положенцев Евгений <apollo64@gmail.com>
2019-09-25 20:40:19 +0600

    #48 убраны лишние поля в forms.py, установил ListView в views.py, стилистика добавлена в detail.html, create.html

--- pawnshop_crm/accounts/forms.py
+++ pawnshop_crm/accounts/forms.py
@@ -3,13 +3,21 @@ from django import forms
 from accounts.models import User
 
 USER_TYPES = [
-        ("0", "Выберите тип прав"),
-        ("1", "Администратор"),
-        ("2", "Кассир_оценщик"),
-        ("3", "Аудитор")
-        ]
+    ("не указана", "Выберите тип прав"),
+    ("Администратор", "Администратор"),
+    ("Кассир-оценщик", "Кассир-оценщик"),
+    ("Аудитор", "Аудитор")
+]
+
 
 class UserForm(forms.ModelForm):
+    username = forms.CharField(
+        label='Логин',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите логин'
+        })
+    )
     first_name = forms.CharField(
         label='Имя',
         widget=forms.TextInput(attrs={
@@ -34,66 +42,9 @@ class UserForm(forms.ModelForm):
     )
     user_type = forms.ChoiceField(
         label='Должность',
-        choices = USER_TYPES,
+        choices=USER_TYPES,
         required=True,
         widget=forms.Select()
-    )
-    # birth_date = forms.DateField(
-    #     label='Дата рождения',
-    #     widget=forms.SelectDateWidget
-    # )
-    # location = forms.CharField(
-    #     label='Населенный пункт',
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'form-control',
-    #         'placeholder': 'Введите название населенного пункта'
-    #     })
-    # )
-    # street = forms.CharField(
-    #     label='Улица',
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'form-control',
-    #         'placeholder': 'Введите название улицы'
-    #     })
-    # )
-    # house_number = forms.CharField(
-    #     label='Номер дома',
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'form-control',
-    #         'placeholder': 'Введите номер дома'
-    #     })
-    # )
-    # apartment_number = forms.CharField(
-    #     label='Номер квартиры',
-    #     required=False,
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'form-control',
-    #         'placeholder': 'Введите номер квартиры'
-    #     })
-    # )
-    # actual_address = forms.CharField(
-    #     label='Фактический адрес проживания',
-    #     required=False,
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'form-control',
-    #         'placeholder': 'Введите фактический адрес проживания'
-    #     })
-    # )
-    # birth_place = forms.CharField(
-    #     label='Место рождения',
-    #     required=False,
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'form-control',
-    #         'placeholder': 'Введите место рождения'
-    #     })
-    # )
-
-    phone = forms.CharField(
-        label='Номер телефона',
-        widget=forms.NumberInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите номер телефона'
-        })
     )
     email = forms.EmailField(
         label='Email',
@@ -102,23 +53,7 @@ class UserForm(forms.ModelForm):
             'placeholder': 'Введите email'
         })
     )
-    # citizenship = forms.CharField(
-    #     label='Гражданство',
-    #     widget=forms.TextInput(attrs={
-    #         'class': 'form-control',
-    #         'placeholder': 'Введите гражданство'
-    #     })
-    # )
-    # files = forms.FileField(
-    #     label='Файлы',
-    #     required=False,
-    #     widget=forms.FileInput(
-    #         attrs={
-    #             'class': 'form-control-file',
-    #         }
-    #     )
-    # )
 
     class Meta:
         model = User
-        exclude = []
+        fields = ['username', 'first_name', 'middle_name', 'last_name', 'email']
--- pawnshop_crm/accounts/views.py
+++ pawnshop_crm/accounts/views.py
@@ -8,6 +8,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from accounts.forms import UserForm
 from accounts.models import User
 
+
 def user_login_view(request):
     context = {}
     if request.method == 'POST':
@@ -26,44 +27,36 @@ def user_login_view(request):
             context['has_error'] = True
     return render(request, 'user/login.html', context=context)
 
+
 def logout_view(request):
     logout(request)
     return redirect('accounts:login')
 
-class UserDetailView(DetailView):
-    template_name = 'user/detail.html'
-    pk_url_kwarg = 'user_pk'
+
+class UserDetailView(ListView):
     model = User
+    template_name = 'user/detail.html'
+    context_object_name = 'users'
+
 
 class UserCreateView(CreateView):
     template_name = 'user/create.html'
     model = User
     form_class = UserForm
-    
+
     def form_valid(self, form):
         data = form.cleaned_data
-        user = User.objects.create(first_name=data['first_name'],
-                                   middle_name=data['middle_name'],
-                                   last_name=data['last_name'],
-                                   email=data['email'],
-                                   user_type=data['user_type'],
-                                   initial_password_changed_at = data['initial_password_changed_at']
-                                   )
+        user = User.objects.create(
+            username=data['username'],
+            first_name=data['first_name'],
+            middle_name=data['middle_name'],
+            last_name=data['last_name'],
+            email=data['email'],
+            user_type=data['user_type'],
+
+        )
         self.object = user
         return redirect(self.get_success_url())
 
-
-
     def get_success_url(self):
-        return reverse('accounts:detail', kwargs={'user_pk': 1})
-
-
-    # def get_context_data(self, **kwargs):
-    #     recent_client_pk = self.request.GET.get('recent_client_pk')
-    #     if recent_client_pk:
-    #         recent_client = get_object_or_404(User, pk=self.request.GET.get('recent_client_pk'))
-    #         kwargs['recent_client'] = recent_client
-    #     return super().get_context_data(**kwargs)
-    # 
-    # def get_success_url(self):
-    #     return reverse('pawnshop:confirm_document_create', kwargs={'client_pk': self.object.pk})
+        return reverse('accounts:user_detail', kwargs={'user_pk': 1})
