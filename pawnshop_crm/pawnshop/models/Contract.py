b935f452ad9c470220fdcfaf32dadd978782909b
Положенцев Евгений <apollo64@gmail.com>
2019-10-14 19:26:16 +0600

    #88 Использовал Loan.id(вместо Contract.ticket_number) для использования в номере залог билета в списке кредитов.

--- pawnshop_crm/pawnshop/models/Contract.py
+++ pawnshop_crm/pawnshop/models/Contract.py
@@ -8,9 +8,9 @@ class Contract(models.Model):
         null=False,
         blank=False
     )
-    loan = models.ForeignKey(
-        'pawnshop.Client',
-        related_name='contract',
+    loan = models.OneToOneField(
+        'pawnshop.Loan',
+        related_name='contract_loan',
         on_delete=models.PROTECT,
         verbose_name='кредит',
         null=False,
@@ -18,8 +18,8 @@ class Contract(models.Model):
         default=None
     )
 
-    ticket_number = models.ForeignKey(
-        'Contract',
+    ticket_number = models.OneToOneField(
+        'pawnshop.Contract',
         related_name='contract',
         on_delete=models.CASCADE,
         verbose_name='номер залогового билета',
@@ -27,7 +27,7 @@ class Contract(models.Model):
         blank=False,
         default=None
     )
-    client = models.ForeignKey(
+    client = models.OneToOneField(
         'pawnshop.Client',
         related_name='contracts',
         on_delete=models.PROTECT,
@@ -41,5 +41,5 @@ class Contract(models.Model):
         verbose_name = 'Залоговый билет'
         verbose_name_plural = 'залоговые билеты'
 
-    def __str__(self):
-        return f'Залоговый билет № \'{self.ticket_number}'
+    # def __str__(self):
+    #     return f'Залоговый билет № {self.ticket_number}'
