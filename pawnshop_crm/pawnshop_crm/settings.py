21a12d2cefa5b61ffedb0d2cf016f2c62cedf086
Положенцев Евгений <apollo64@gmail.com>
2019-10-28 14:36:20 +0600

    #99 В  settings.py установлен ALLOWED_HOSTS = ['*'].

--- pawnshop_crm/pawnshop_crm/settings.py
+++ pawnshop_crm/pawnshop_crm/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'usq#tr2q2dh5d$x$3vukvhm0kh831pgabk_+&v)zvuj(*a8ug-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
