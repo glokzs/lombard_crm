43d57af264a3b6f83496f5126038f96dcef45ff4
Положенцев Евгений <apollo64@gmail.com>
2019-11-06 19:16:00 +0600

    #99 Изменены заголовки в login.feature и test_login.py

--- pawnshop_crm/test-pytest/features/login.feature
+++ pawnshop_crm/test-pytest/features/login.feature
@@ -4,11 +4,11 @@ Feature: Login as admin
 Scenario: Login as admin
     Given Create superuser
     And Login page
-    Then I logging as admin
-    Then I should see admin page
-    Then I click "Пользователи"
-    Then I should see "Выберите пользователь для изменения"
-    Then I click "Добавить пользователя"
-    Then I should see user create menu
-    Then I enter new user info
-    Then I should see user successful creation
+    Then I login as admin
+    And I should see admin page
+    And I click "Пользователи"
+    And I should see "Выберите пользователь для изменения"
+    And I click "Добавить пользователя"
+    And I should see user create menu
+    And I enter new user info
+    And I should see user successful creation
--- pawnshop_crm/test-pytest/features/test_login.py
+++ pawnshop_crm/test-pytest/features/test_login.py
@@ -5,37 +5,43 @@ from pytest_bdd import scenario, given, then
 def test_login(live_server):
     return live_server
 
-@given ('Create superuser')
+
+@given('Create superuser')
 def super_user(admin_user):
     return admin_user
 
+
 @given('Login page')
 def go_to_login_page(browser, live_server):
-    browser.visit(live_server.url +'/admin')
+    browser.visit(live_server.url + '/admin')
 
 
-@then('I logging as admin')
+@then('I login as admin')
 def admin_login(browser):
     browser.fill('username', 'admin')
     browser.fill('password', 'password')
     browser.driver.find_element_by_class_name('submit-row').click()
 
+
 @then('I should see admin page')
 def admin_index(browser):
     assert 'Pawnshop' in browser.html
-    # assert browser.status_code == 200
+
 
 @then('I click "Пользователи"')
 def click_users(browser):
     browser.driver.find_element_by_link_text('Пользователи').click()
 
+
 @then('I should see "Выберите пользователь для изменения"')
 def users_index(browser):
     assert 'Выберите пользователь для изменения' in browser.html
 
+
 @then('I click "Добавить пользователя"')
 def click_create_users(browser):
     browser.driver.find_element_by_class_name('addlink').click()
+
 
 @then('I should see user create menu')
 def create_index(browser):
@@ -43,7 +49,7 @@ def create_index(browser):
 
 
 @then('I enter new user info')
-def Create_user_form(browser):
+def create_user_form(browser):
     browser.fill('username', 'testuser')
     browser.fill('password1', 'xsw2.1qaz')
     browser.fill('password2', 'xsw2.1qaz')
@@ -53,7 +59,9 @@ def Create_user_form(browser):
     browser.fill('user-0-email', 'test@test.ts')
     browser.driver.find_element_by_name('_save').click()
 
+
 @then('I should see user successful creation')
 def creation_success(browser):
-    assert 'было успешно добавлено.' in browser.html
+    assert 'было успешно добавлено.' in browser.html, "Пожалуйста, исправьте ошибки ниже."
+
 
