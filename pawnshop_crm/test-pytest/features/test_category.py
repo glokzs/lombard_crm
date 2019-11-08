f0f16a4b0d9a7326656586dd208c3282d4be23f3
Положенцев Евгений <apollo64@gmail.com>
2019-11-08 19:17:18 +0600

    #123 Изменены заголовки функций и изменен gitignore

--- pawnshop_crm/test-pytest/features/test_category.py
+++ pawnshop_crm/test-pytest/features/test_category.py
@@ -15,7 +15,7 @@ def go_to_login_page(browser, live_server):
 
 
 @then('I login as admin')
-def admin_login(browser):
+def admin_log_in(browser):
     browser.fill('username', 'admin')
     browser.fill('password', 'password')
     browser.driver.find_element_by_class_name('submit-row').click()
@@ -34,49 +34,49 @@ def category_index(browser):
 
 
 @then('I click creation option')
-def click_create_users(browser):
+def click_create_catefory(browser):
     browser.driver.find_element_by_class_name('addlink').click()
 
 @then('I should see form')
-def category_index(browser):
+def category_form(browser):
     assert 'Добавить Категория' in browser.html
 
 @then('I filling name and tariff and save form')
-def admin_login(browser):
+def fill_form(browser):
     browser.fill('name', 'Тест категория')
     browser.fill('interest_rate', '0,9')
     browser.driver.find_element_by_name('_save').click()
 
 @then('I should see Category list and success message')
-def category_index(browser):
+def success_message(browser):
     assert 'было успешно добавлено' in browser.html
 
 @then('I click main admin menu')
-def admin_page_category(browser):
+def click_admin_main_menu(browser):
     browser.driver.find_element_by_link_text('Начало').click()
 
 @then('I should see admin page')
-def admin_index(browser):
+def see_admin_index(browser):
     assert 'Pawnshop' in browser.html
 
 @then('I click subcategory option')
-def admin_page_category(browser):
+def admin_page_subcategory(browser):
     browser.driver.find_element_by_link_text('Подкатегории').click()
 
 @then('I should see subcategory menu')
-def category_index(browser):
+def subcategory_menu(browser):
     assert 'Подкатегория' in browser.html
 
 @then('I click create subcategory option')
-def click_create_users(browser):
+def click_create_subcategory(browser):
     browser.driver.find_element_by_class_name('addlink').click()
 
 @then('I should see create subcategory form')
-def category_index(browser):
+def see_subcategory_creattion_form(browser):
     assert 'Добавить Подкатегория' in browser.html
 
 @then('I filling form and click save')
-def admin_login(browser):
+def fill_subcategory_form(browser):
     select_elem = browser.driver.find_element_by_name('category')
     select_elem.click()
     options = select_elem.find_elements_by_tag_name('option')
@@ -85,5 +85,5 @@ def admin_login(browser):
     browser.driver.find_element_by_name('_save').click()
 
 @then('I should see success creation page')
-def category_index(browser):
+def success_message(browser):
     assert 'было успешно добавлено' in browser.html
