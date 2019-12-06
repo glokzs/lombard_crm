b7d67360d1b0d5f14e03995f8604920b549a09c7
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-06 19:41:39 +0600

    #154 Добавлен fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+import os
+
+from fabric import task
+
+PROJECT_DIR = '/home/projects/lombard_crm'
+VIRTUALENV_ACTIVATE_CMD = 'source /home/projects/lombard_crm/venv/bin/activate'
+USERNAME = os.environ.get('username')
+HOSTNAME = os.environ.get('host')
+hosts = [f'{USERNAME}@{HOSTNAME}']
+
+
+@task(hosts=hosts)
+def deploy(connection):
+    connection.put(
+        'docker/docker-compose.yml',
+        f'{PROJECT_DIR}/docker-compose.yml'
+    )
+    connection.put(
+        'docker/nginx.conf',
+        f'{PROJECT_DIR}/nginx.conf'
+    )
+    connection.put(
+        'docker/gunicorn.conf',
+        f'{PROJECT_DIR}/gunicorn.conf'
+    )
+    connection.run(
+        f'cd {PROJECT_DIR}; docker-compose pull && docker-compose down && docker-compose up -d'
+    )
