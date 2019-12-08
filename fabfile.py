d55d4fb928e6cc90a29a1777e505c889c83a557a
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-08 18:53:48 +0600

    #154 Image в сервисе django изменен на qwanysh/lombard_crm

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,8 @@ PROJECT_DIR = '/home/projects/lombard_crm'
 VIRTUALENV_ACTIVATE_CMD = 'source /home/projects/lombard_crm/venv/bin/activate'
 USERNAME = os.environ.get('username')
 HOSTNAME = os.environ.get('host')
+DOCKER_USERNAME = os.environ.get('DOCKER_USERNAME')
+DOCKER_PASSWORD = os.environ.get('DOCKER_PASSWORD')
 hosts = [f'{USERNAME}@{HOSTNAME}']
 
 
@@ -22,6 +24,9 @@ def deploy(connection):
     connection.put(
         'docker/gunicorn.conf',
         f'{PROJECT_DIR}/gunicorn.conf'
+    )
+    connection.run(
+        f'cd {PROJECT_DIR}; docker login -u {DOCKER_USERNAME} -p {DOCKER_PASSWORD}'
     )
     connection.run(
         f'cd {PROJECT_DIR}; docker-compose pull && docker-compose down && docker-compose up -d'
