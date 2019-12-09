471e16add220f7c6bdc52fbfbb7821f2b7d8fee5
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-09 11:35:56 +0600

    #156 Удален вывод total amount со страницы списка операций

--- pawnshop_crm/pawnshop/views/operation_views.py
+++ pawnshop_crm/pawnshop/views/operation_views.py
@@ -10,13 +10,5 @@ class OperationListView(UserPassesTestMixin, ListView):
     model = Operation
     ordering = ['-created_at']
 
-    def get_context_data(self, **kwargs):
-        VALUE_IF_NONE = 0
-
-        total_amount = Operation.objects.aggregate(sum=Sum("amount"))['sum']
-        kwargs['total_amount'] = total_amount if total_amount else VALUE_IF_NONE
-
-        return super().get_context_data(**kwargs)
-
     def test_func(self):
         return self.request.user.has_perm('accounts.operations_view')
